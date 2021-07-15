96b74449789e54ec6ac4196eff91615b1b748360
favonia <favonia@gmail.com>
2021-07-15 11:25:44 -0500

    Slightly improve the pretty-printer for time durations.

--- internal/cron/printer.go
+++ internal/cron/printer.go
@@ -6,9 +6,12 @@ import (
 )
 
 func PPDuration(d time.Duration) string {
-	if d < time.Second {
+	switch {
+	case d <= 0:
+		return "no time "
+	case d < time.Second:
 		return "less than 1s"
+	default:
+		return fmt.Sprintf("about %v", d.Round(time.Second))
 	}
-
-	return fmt.Sprintf("about %v", d.Round(time.Second))
 }
