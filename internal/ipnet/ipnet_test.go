cd20d4f687f4d645f8bec2313fc0add7e7f64d4d
favonia <favonia@gmail.com>
2021-08-08 11:32:04 -0500

    refactor(ipnet): add ipnet.NormalizeIP

--- internal/ipnet/ipnet_test.go
+++ internal/ipnet/ipnet_test.go
@@ -1,9 +1,10 @@
 package ipnet_test
 
 import (
+	"net"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/favonia/cloudflare-ddns/internal/ipnet"
 )
@@ -11,8 +12,8 @@ import (
 func TestString(t *testing.T) {
 	t.Parallel()
 	for name, tc := range map[string]struct {
-		input  ipnet.Type
-		output string
+		input    ipnet.Type
+		expected string
 	}{
 		"4":   {ipnet.IP4, "IPv4"},
 		"6":   {ipnet.IP6, "IPv6"},
@@ -21,7 +22,7 @@ func TestString(t *testing.T) {
 		tc := tc
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
-			assert.Equal(t, tc.output, tc.input.String())
+			require.Equal(t, tc.expected, tc.input.String())
 		})
 	}
 }
@@ -29,8 +30,8 @@ func TestString(t *testing.T) {
 func TestRecordType(t *testing.T) {
 	t.Parallel()
 	for name, tc := range map[string]struct {
-		input  ipnet.Type
-		output string
+		input    ipnet.Type
+		expected string
 	}{
 		"4":   {ipnet.IP4, "A"},
 		"6":   {ipnet.IP6, "AAAA"},
@@ -39,7 +40,7 @@ func TestRecordType(t *testing.T) {
 		tc := tc
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
-			assert.Equal(t, tc.output, tc.input.RecordType())
+			require.Equal(t, tc.expected, tc.input.RecordType())
 		})
 	}
 }
@@ -47,8 +48,8 @@ func TestRecordType(t *testing.T) {
 func TestInt(t *testing.T) {
 	t.Parallel()
 	for name, tc := range map[string]struct {
-		input  ipnet.Type
-		output int
+		input    ipnet.Type
+		expected int
 	}{
 		"4":   {ipnet.IP4, 4},
 		"6":   {ipnet.IP6, 6},
@@ -57,7 +58,29 @@ func TestInt(t *testing.T) {
 		tc := tc
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
-			assert.Equal(t, tc.output, tc.input.Int())
+			require.Equal(t, tc.expected, tc.input.Int())
+		})
+	}
+}
+
+func TestNormalizeIP(t *testing.T) {
+	t.Parallel()
+	for name, tc := range map[string]struct {
+		ipNet    ipnet.Type
+		ip       net.IP
+		expected net.IP
+	}{
+		"4-1::2":             {ipnet.IP4, net.ParseIP("1::2").To16(), nil},
+		"4-::ffff:0a0a:0a0a": {ipnet.IP4, net.ParseIP("::ffff:0a0a:0a0a").To16(), net.ParseIP("10.10.10.10").To4()},
+		"6-1::2":             {ipnet.IP6, net.ParseIP("1::2").To16(), net.ParseIP("1::2").To16()},
+		"6-10.10.10.10":      {ipnet.IP6, net.ParseIP("10.10.10.10").To4(), net.ParseIP("10.10.10.10").To16()},
+		"100-nil":            {100, nil, nil},
+		"100-10.10.10.10":    {100, net.ParseIP("10.10.10.10").To4(), net.ParseIP("10.10.10.10").To4()},
+	} {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			require.Equal(t, tc.expected, tc.ipNet.NormalizeIP(tc.ip))
 		})
 	}
 }
