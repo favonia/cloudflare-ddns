891c9adf0373fae9a181f79b7b9201e79ddeefd0
favonia <favonia@gmail.com>
2021-08-09 12:48:36 -0500

    test(config): ReadAuth

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -18,12 +18,36 @@ func TestDefaultConfigNotNil(t *testing.T) {
 
 //nolint: paralleltest // environment vars are global
 func TestReadAuthToken(t *testing.T) {
-	set("CF_API_TOKEN", "123456789")
+	unset("CF_API_TOKEN")
 	unset("CF_API_TOKEN_FILE")
-	set("CF_ACCOUNT_ID", "secret account")
+	unset("CF_ACCOUNT_ID")
 
-	var field api.Auth
-	ok := config.ReadAuth(quiet.QUIET, 2, &field)
-	require.True(t, ok)
-	require.Equal(t, &api.CloudflareAuth{Token: "123456789", AccountID: "secret account", BaseURL: ""}, field)
+	for name, tc := range map[string]struct {
+		token   string
+		account string
+		ok      bool
+	}{
+		"full":      {"123456789", "secret account", false},
+		"noaccount": {"123456789", "", false},
+		"notoken":   {"", "account", true},
+	} {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			set("CF_API_TOKEN", tc.token)
+			set("CF_ACCOUNT_ID", tc.account)
+			defer unset("CF_API_TOKEN")
+			defer unset("CF_ACCOUNT_ID")
+
+			var field api.Auth
+			ok := config.ReadAuth(quiet.QUIET, 2, &field)
+			if tc.ok {
+				require.False(t, ok)
+				require.Nil(t, field)
+			} else {
+				require.True(t, ok)
+				require.Equal(t, &api.CloudflareAuth{Token: tc.token, AccountID: tc.account, BaseURL: ""}, field)
+			}
+
+		})
+	}
 }
