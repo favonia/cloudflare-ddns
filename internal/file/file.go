b27ac5631fc8ceef05405ec6a71252db1d74553c
favonia <favonia@gmail.com>
2022-06-18 05:49:32 -0500

    fix(file): accept absolute paths

--- internal/file/file.go
+++ internal/file/file.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"io/fs"
 	"os"
+	"path/filepath"
 
 	"github.com/favonia/cloudflare-ddns/internal/pp"
 )
@@ -11,6 +12,16 @@ import (
 var FS = os.DirFS("/") //nolint:gochecknoglobals
 
 func ReadString(ppfmt pp.PP, path string) (string, bool) {
+	// os.DirFS(...).Open() does not accept absolute paths
+	if filepath.IsAbs(path) {
+		newpath, err := filepath.Rel("/", path)
+		if err != nil {
+			ppfmt.Errorf(pp.EmojiImpossible, `%q is an absolute path but does not start with "/": %v`, err)
+			return "", false
+		}
+		path = newpath
+	}
+
 	body, err := fs.ReadFile(FS, path)
 	if err != nil {
 		ppfmt.Errorf(pp.EmojiUserError, "Failed to read %q: %v", path, err)
--- internal/file/file_test.go
+++ internal/file/file_test.go
@@ -49,7 +49,7 @@ func TestReadStringWrongPath(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	useMemFS(t, fstest.MapFS{})
 
-	path := "/wrong/path.txt"
+	path := "wrong/path.txt"
 	mockPP := mocks.NewMockPP(mockCtrl)
 	mockPP.EXPECT().Errorf(pp.EmojiUserError, "Failed to read %q: %v", path, gomock.Any())
 	content, ok := file.ReadString(mockPP, path)
@@ -74,4 +74,27 @@ func TestReadStringNoAccess(t *testing.T) {
 	content, ok := file.ReadString(mockPP, "dir")
 	require.False(t, ok)
 	require.Empty(t, content)
+}
+
+//nolint:paralleltest // reading global var file.FS
+func TestReadStringOkayAbsolutePath(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+
+	path := "test/file.txt"
+	written := " hello world   " // space is intentionally added to test trimming
+	expected := strings.TrimSpace(written)
+
+	useMemFS(t, fstest.MapFS{
+		path: &fstest.MapFile{
+			Data:    []byte(written),
+			Mode:    0o644,
+			ModTime: time.Unix(1234, 5678),
+			Sys:     nil,
+		},
+	})
+
+	mockPP := mocks.NewMockPP(mockCtrl)
+	content, ok := file.ReadString(mockPP, "/"+path)
+	require.True(t, ok)
+	require.Equal(t, expected, content)
 }
