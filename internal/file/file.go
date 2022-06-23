55c598831b15094b7edd9928bc89bba0cc1a048b
favonia <favonia@gmail.com>
2022-06-23 01:56:19 -0500

    fix(file): fix arguments of pp.Errorf

--- internal/file/file.go
+++ internal/file/file.go
@@ -9,14 +9,16 @@ import (
 	"github.com/favonia/cloudflare-ddns/internal/pp"
 )
 
-var FS = os.DirFS("/") //nolint:gochecknoglobals
+const LinuxRoot string = "/"
+
+var FS = os.DirFS(LinuxRoot) //nolint:gochecknoglobals
 
 func ReadString(ppfmt pp.PP, path string) (string, bool) {
 	// os.DirFS(...).Open() does not accept absolute paths
 	if filepath.IsAbs(path) {
-		newpath, err := filepath.Rel("/", path)
+		newpath, err := filepath.Rel(LinuxRoot, path)
 		if err != nil {
-			ppfmt.Errorf(pp.EmojiImpossible, `%q is an absolute path but does not start with "/": %v`, err)
+			ppfmt.Errorf(pp.EmojiImpossible, `%q is an absolute path but does not start with %q: %v`, path, LinuxRoot, err)
 			return "", false
 		}
 		path = newpath
