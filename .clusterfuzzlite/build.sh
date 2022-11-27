2b0165df2a0cf0c39a71b5638b6d17e3204a5eba
favonia <favonia@gmail.com>
2022-11-26 19:21:27 -0600

    test: revert back to Go 1.18 native fuzzing

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -eu
 
-compile_go_fuzzer github.com/favonia/cloudflare-ddns/test/fuzzer ParseList fuzz_parse_list
-compile_go_fuzzer github.com/favonia/cloudflare-ddns/test/fuzzer ParseExpression fuzz_parse_expression
+go install github.com/AdamKorcz/go-118-fuzz-build@v0.0.0-20221121202950-b2031950a318
+go get github.com/AdamKorcz/go-118-fuzz-build/testing@v0.0.0-20221121202950-b2031950a318
+
+compile_native_go_fuzzer github.com/favonia/cloudflare-ddns/test/fuzzer FuzzParseList fuzz_parse_list
+compile_native_go_fuzzer github.com/favonia/cloudflare-ddns/test/fuzzer FuzzParseExpression fuzz_parse_expression
