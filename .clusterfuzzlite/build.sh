0083bf0cfddadb9c5d1aed0c674c07dc385a25fe
favonia <favonia@gmail.com>
2023-01-02 08:31:28 -0600

    fix: remove files related to ClusterFuzz Lite (#348)

--- .clusterfuzzlite/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -eu
-
-go install github.com/AdamKorcz/go-118-fuzz-build@v0.0.0-20221121202950-b2031950a318
-go get github.com/AdamKorcz/go-118-fuzz-build/testing@v0.0.0-20221121202950-b2031950a318
-
-compile_native_go_fuzzer github.com/favonia/cloudflare-ddns/test/fuzzer FuzzParseList fuzz_parse_list
-compile_native_go_fuzzer github.com/favonia/cloudflare-ddns/test/fuzzer FuzzParseExpression fuzz_parse_expression
