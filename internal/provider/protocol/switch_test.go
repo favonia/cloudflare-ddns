402c538965685cb043e9c417ee96f9b4d3dbb506
favonia <favonia@gmail.com>
2023-05-22 06:28:48 -0400

    ci: make linter happy

--- internal/provider/protocol/switch_test.go
+++ internal/provider/protocol/switch_test.go
@@ -9,12 +9,14 @@ import (
 )
 
 func TestConstant(t *testing.T) {
+	t.Parallel()
 	s := protocol.Constant("very secret string")
 	require.Equal(t, s.Switch(false), "very secret string")
 	require.Equal(t, s.Switch(true), "very secret string")
 }
 
 func TestSwitchable(t *testing.T) {
+	t.Parallel()
 	s := protocol.Switchable{Use1001: "very secret string 1", Use1111: "very secret string 2"}
 	require.Equal(t, s.Switch(false), "very secret string 2")
 	require.Equal(t, s.Switch(true), "very secret string 1")
