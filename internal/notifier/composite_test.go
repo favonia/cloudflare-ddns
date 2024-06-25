14be06d479239effe1c4b93fe4b0da4dd65ba45c
favonia <favonia@gmail.com>
2024-06-24 19:58:31 -0700

    test(notifier): add tests

--- internal/notifier/composite_test.go
+++ internal/notifier/composite_test.go
@@ -2,12 +2,14 @@ package notifier_test
 
 import (
 	"context"
+	"strings"
 	"testing"
 
 	"go.uber.org/mock/gomock"
 
 	"github.com/favonia/cloudflare-ddns/internal/mocks"
 	"github.com/favonia/cloudflare-ddns/internal/notifier"
+	"github.com/favonia/cloudflare-ddns/internal/response"
 )
 
 func TestDescribeAll(t *testing.T) {
@@ -44,4 +46,40 @@ func TestSendAll(t *testing.T) {
 	}
 
 	notifier.SendAll(context.Background(), mockPP, ms, message)
+}
+
+func TestSendResponseAll(t *testing.T) {
+	t.Parallel()
+
+	monitorMessages := []string{"forest", "grass"}
+	notifierMessages := []string{"ocean", "moon"}
+	notifierMessage := strings.Join(notifierMessages, " ")
+
+	for name, tc := range map[string]struct {
+		ok bool
+	}{
+		"ok":    {true},
+		"notok": {false},
+	} {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			ns := make([]notifier.Notifier, 0, 5)
+			mockCtrl := gomock.NewController(t)
+			mockPP := mocks.NewMockPP(mockCtrl)
+
+			for range 5 {
+				n := mocks.NewMockNotifier(mockCtrl)
+				n.EXPECT().Send(context.Background(), mockPP, notifierMessage)
+				ns = append(ns, n)
+			}
+
+			resp := response.Response{
+				Ok:               tc.ok,
+				MonitorMessages:  monitorMessages,
+				NotifierMessages: notifierMessages,
+			}
+			notifier.SendResponseAll(context.Background(), mockPP, ns, resp)
+		})
+	}
 }
