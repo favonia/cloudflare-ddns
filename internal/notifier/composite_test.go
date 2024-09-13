favonia/cloudflare-ddns

commit 319e6a0d266a060037c08080ceb2495df600d63f

refactor: rewrite code using Go 1.23 iterators (#911)

internal/config/config.go
@@ -31,8 +31,8 @@ type Config struct {
 	WAFListDescription string
 	DetectionTimeout   time.Duration
 	UpdateTimeout      time.Duration
-	Monitors           []monitor.Monitor
-	Notifiers          []notifier.Notifier
+	Monitor            monitor.Monitor
+	Notifier           notifier.Notifier
 }
 
 // Default gives the default configuration.
@@ -59,7 +59,7 @@ func Default() *Config {
 		WAFListDescription: "",
 		DetectionTimeout:   time.Second * 5,  //nolint:mnd
 		UpdateTimeout:      time.Second * 30, //nolint:mnd
-		Monitors:           nil,
-		Notifiers:          nil,
+		Monitor:            nil,
+		Notifier:           nil,
 	}
 }

internal/config/config_print.go
@@ -10,8 +10,6 @@ import (
 	"github.com/favonia/cloudflare-ddns/internal/cron"
 	"github.com/favonia/cloudflare-ddns/internal/domain"
 	"github.com/favonia/cloudflare-ddns/internal/ipnet"
-	"github.com/favonia/cloudflare-ddns/internal/monitor"
-	"github.com/favonia/cloudflare-ddns/internal/notifier"
 	"github.com/favonia/cloudflare-ddns/internal/pp"
 	"github.com/favonia/cloudflare-ddns/internal/provider"
 )
@@ -88,17 +86,17 @@ func (c *Config) Print(ppfmt pp.PP) {
 	item("IP detection:", "%v", c.DetectionTimeout)
 	item("Record/list updating:", "%v", c.UpdateTimeout)
 
-	if len(c.Monitors) > 0 {
+	if c.Monitor != nil {
 		section("Monitors:")
-		monitor.DescribeAll(func(service, params string) {
-			item(service+":", "%s", params)
-		}, c.Monitors)
+		for name, params := range c.Monitor.Describe {
+			item(name+":", "%s", params)
+		}
 	}
 
-	if len(c.Notifiers) > 0 {
+	if c.Notifier != nil {
 		section("Notification services (via shoutrrr):")
-		notifier.DescribeAll(func(service, params string) {
-			item(service+":", "%s", params)
-		}, c.Notifiers)
+		for name, params := range c.Notifier.Describe {
+			item(name+":", "%s", params)
+		}
 	}
 }

internal/config/env_notifier.go
@@ -6,7 +6,7 @@ import (
 )
 
 // ReadAndAppendShoutrrrURL reads the URLs separated by the newline.
-func ReadAndAppendShoutrrrURL(ppfmt pp.PP, key string, field *[]notifier.Notifier) bool {
+func ReadAndAppendShoutrrrURL(ppfmt pp.PP, key string, field *notifier.Notifier) bool {
 	vals := GetenvAsList(key, "\n")
 	if len(vals) == 0 {
 		return true
@@ -18,6 +18,6 @@ func ReadAndAppendShoutrrrURL(ppfmt pp.PP, key string, field *[]notifier.Notifie
 	}
 
 	// Append the new monitor to the existing list
-	*field = append(*field, s)
+	*field = s
 	return true
 }

internal/monitor/composite.go
@@ -3,89 +3,79 @@ package monitor
 import (
 	"context"
 
-	"github.com/favonia/cloudflare-ddns/internal/message"
 	"github.com/favonia/cloudflare-ddns/internal/pp"
 )
 
-// DescribeAll calls [Monitor.Describe] for each monitor in the group with the callback.
-func DescribeAll(callback func(service, params string), ms []Monitor) {
+type monitors []BasicMonitor
+
+var _ Monitor = monitors{}
+
+// NewComposed creates a new composed monitor.
+func NewComposed(mons ...BasicMonitor) monitors {
+	ms := make([]BasicMonitor, 0, len(mons))
+	for _, m := range mons {
+		if m == nil {
+			continue
+		}
+		if list, composed := m.(monitors); composed {
+			ms = append(ms, list...)
+		} else {
+			ms = append(ms, m)
+		}
+	}
+	return monitors(ms)
+}
+
+// Describe calls [Monitor.Describe] for each monitor in the group with the callback.
+func (ms monitors) Describe(yield func(name string, params string) bool) {
 	for _, m := range ms {
-		m.Describe(callback)
+		for name, params := range m.Describe {
+			if !yield(name, params) {
+				return
+			}
+		}
 	}
 }
 
-// SuccessAll calls [Monitor.Success] for each monitor in the group.
-func SuccessAll(ctx context.Context, ppfmt pp.PP, ms []Monitor, message string) bool {
+// Ping calls [Monitor.Ping] for each monitor in the group.
+func (ms monitors) Ping(ctx context.Context, ppfmt pp.PP, message Message) bool {
 	ok := true
 	for _, m := range ms {
-		if !m.Success(ctx, ppfmt, message) {
-			ok = false
+		ok = ok && m.Ping(ctx, ppfmt, message)
+	}
+	return ok
+}
+
+// Start calls [Monitor.Start] for each monitor in ms.
+func (ms monitors) Start(ctx context.Context, ppfmt pp.PP, message string) bool {
+	ok := true
+	for _, m := range ms {
+		if em, extended := m.(Monitor); extended {
+			ok = ok && em.Start(ctx, ppfmt, message)
 		}
 	}
 	return ok
 }
 
-// StartAll calls [Monitor.Start] for each monitor in ms.
-func StartAll(ctx context.Context, ppfmt pp.PP, ms []Monitor, message string) bool {
+// Exit calls [Monitor.Exit] for each monitor in ms.
+func (ms monitors) Exit(ctx context.Context, ppfmt pp.PP, message string) bool {
 	ok := true
 	for _, m := range ms {
-		if !m.Start(ctx, ppfmt, message) {
-			ok = false
+		if em, extended := m.(Monitor); extended {
+			ok = ok && em.Exit(ctx, ppfmt, message)
 		}
 	}
 	return ok
 }
 
-// FailureAll calls [Monitor.Failure] for each monitor in ms.
-func FailureAll(ctx context.Context, ppfmt pp.PP, ms []Monitor, message string) bool {
+// Log calls [Monitor.Log] for each monitor in the group.
+func (ms monitors) Log(ctx context.Context, ppfmt pp.PP, msg Message) bool {
 	ok := true
 	for _, m := range ms {
-		if !m.Failure(ctx, ppfmt, message) {
-			ok = false
-		}
-	}
-	return ok
-}
-
-// LogAll calls [Monitor.Log] for each monitor in ms.
-func LogAll(ctx context.Context, ppfmt pp.PP, ms []Monitor, message string) bool {
-	ok := true
-	for _, m := range ms {
-		if !m.Log(ctx, ppfmt, message) {
-			ok = false
-		}
-	}
-	return ok
-}
-
-// ExitStatusAll calls [Monitor.ExitStatus] for each monitor in ms.
-func ExitStatusAll(ctx context.Context, ppfmt pp.PP, ms []Monitor, code int, message string) bool {
-	ok := true
-	for _, m := range ms {
-		if !m.ExitStatus(ctx, ppfmt, code, message) {
-			ok = false
-		}
-	}
-	return ok
-}
-
-// PingMessageAll calls [SendMessage] for each monitor in ms.
-func PingMessageAll(ctx context.Context, ppfmt pp.PP, ms []Monitor, msg message.MonitorMessage) bool {
-	ok := true
-	for _, m := range ms {
-		if !PingMessage(ctx, ppfmt, m, msg) {
-			ok = false
-		}
-	}
-	return ok
-}
-
-// LogMessageAll calls [SendMessage] for each monitor in ms.
-func LogMessageAll(ctx context.Context, ppfmt pp.PP, ms []Monitor, msg message.MonitorMessage) bool {
-	ok := true
-	for _, m := range ms {
-		if !LogMessage(ctx, ppfmt, m, msg) {
-			ok = false
+		if em, extended := m.(Monitor); extended {
+			ok = ok && em.Log(ctx, ppfmt, msg)
+		} else if !msg.OK {
+			ok = ok && m.Ping(ctx, ppfmt, msg)
 		}
 	}
 	return ok

internal/notifier/composite.go
@@ -1,37 +1,0 @@
-package notifier
-
-import (
-	"context"
-
-	"github.com/favonia/cloudflare-ddns/internal/message"
-	"github.com/favonia/cloudflare-ddns/internal/pp"
-)
-
-// DescribeAll calls [Notifier.Describe] for each monitor in the group with the callback.
-func DescribeAll(callback func(service, params string), ns []Notifier) {
-	for _, n := range ns {
-		n.Describe(callback)
-	}
-}
-
-// SendAll calls [Notifier.Send] for each monitor in the group.
-func SendAll(ctx context.Context, ppfmt pp.PP, ns []Notifier, message string) bool {
-	ok := true
-	for _, n := range ns {
-		if !n.Send(ctx, ppfmt, message) {
-			ok = false
-		}
-	}
-	return ok
-}
-
-// SendMessageAll calls [SendMessage] for each monitor in the group.
-func SendMessageAll(ctx context.Context, ppfmt pp.PP, ns []Notifier, msg message.NotifierMessage) bool {
-	ok := true
-	for _, n := range ns {
-		if !SendMessage(ctx, ppfmt, n, msg) {
-			ok = false
-		}
-	}
-	return ok
-}

internal/config/env_notifier_test.go
@@ -21,37 +21,35 @@ func TestReadAndAppendShoutrrrURL(t *testing.T) {
 	for name, tc := range map[string]struct {
 		set           bool
 		val           string
-		oldField      []not
-		newField      func(*testing.T, []not)
+		oldField      not
+		newField      func(*testing.T, not)
 		ok            bool
 		prepareMockPP func(*mocks.MockPP)
 	}{
 		"unset": {
 			false, "", nil,
-			func(t *testing.T, ns []not) {
+			func(t *testing.T, n not) {
 				t.Helper()
-				require.Nil(t, ns)
+				require.Nil(t, n)
 			},
 			true, nil,
 		},
 		"empty": {
 			true, "", nil,
-			func(t *testing.T, ns []not) {
+			func(t *testing.T, n not) {
 				t.Helper()
-				require.Nil(t, ns)
+				require.Nil(t, n)
 			},
 			true, nil,
 		},
 		"generic": {
 			true, "generic+https://example.com/api/v1/postStuff",
 			nil,
-			func(t *testing.T, ns []not) {
+			func(t *testing.T, n not) {
 				t.Helper()
-				require.Len(t, ns, 1)
-				m := ns[0]
-				s, ok := m.(notifier.Shoutrrr)
+				s, ok := n.(notifier.Shoutrrr)
 				require.True(t, ok)
-				require.Equal(t, []string{"generic"}, s.ServiceNames)
+				require.Equal(t, []string{"Generic"}, s.ServiceDescriptions)
 			},
 			true,
 			nil,
@@ -59,9 +57,9 @@ func TestReadAndAppendShoutrrrURL(t *testing.T) {
 		"ill-formed": {
 			true, "meow-meow-meow://cute",
 			nil,
-			func(t *testing.T, ns []not) {
+			func(t *testing.T, n not) {
 				t.Helper()
-				require.Nil(t, ns)
+				require.Nil(t, n)
 			},
 			false,
 			func(m *mocks.MockPP) {
@@ -71,13 +69,11 @@ func TestReadAndAppendShoutrrrURL(t *testing.T) {
 		"multiple": {
 			true, "generic+https://example.com/api/v1/postStuff\npushover://shoutrrr:token@userKey",
 			nil,
-			func(t *testing.T, ns []not) {
+			func(t *testing.T, n not) {
 				t.Helper()
-				require.Len(t, ns, 1)
-				m := ns[0]
-				s, ok := m.(notifier.Shoutrrr)
+				s, ok := n.(notifier.Shoutrrr)
 				require.True(t, ok)
-				require.Equal(t, []string{"generic", "pushover"}, s.ServiceNames)
+				require.Equal(t, []string{"Generic", "Pushover"}, s.ServiceDescriptions)
 			},
 			true,
 			nil,

internal/monitor/composite_test.go
@@ -3,56 +3,93 @@ package monitor_test
 import (
 	"context"
 	"fmt"
-	"strings"
 	"testing"
 
+	"github.com/stretchr/testify/require"
 	"go.uber.org/mock/gomock"
 
-	"github.com/favonia/cloudflare-ddns/internal/message"
 	"github.com/favonia/cloudflare-ddns/internal/mocks"
 	"github.com/favonia/cloudflare-ddns/internal/monitor"
 )
 
-func TestDescribeAll(t *testing.T) {
+func TestComposedDescribe(t *testing.T) {
 	t.Parallel()
-
-	ms := make([]monitor.Monitor, 0, 5)
 
 	mockCtrl := gomock.NewController(t)
 
-	for range 5 {
+	ms1 := make([]monitor.BasicMonitor, 0, 5)
+	for range 3 {
 		m := mocks.NewMockMonitor(mockCtrl)
-		m.EXPECT().Describe(gomock.Any())
-		ms = append(ms, m)
+		m.EXPECT().Describe(gomock.Any()).DoAndReturn(
+			func(yield func(string, string) bool) {
+				yield("name", "params")
+			},
+		)
+		ms1 = append(ms1, m)
 	}
+	ms2 := make([]monitor.BasicMonitor, 0, 5)
+	for range 2 {
+		m := mocks.NewMockMonitor(mockCtrl)
+		ms2 = append(ms2, m)
+	}
+	c := monitor.NewComposed(monitor.NewComposed(ms1...), monitor.NewComposed(ms2...))
 
-	callback := func(_service, _params string) { /* the callback content is not relevant here. */ }
-	monitor.DescribeAll(callback, ms)
+	count := 0
+	for range c.Describe {
+		count++
+		if count >= 3 {
+			break
+		}
+	}
+	require.Equal(t, 3, count)
 }
 
-func TestSuccessAll(t *testing.T) {
+func TestComposedPing(t *testing.T) {
 	t.Parallel()
 
-	ms := make([]monitor.Monitor, 0, 5)
+	for name1, tc1 := range map[string]struct {
+		lines []string
+	}{
+		"nil":   {nil},
+		"empty": {[]string{}},
+		"one":   {[]string{"hi"}},
+		"two":   {[]string{"hi", "hey"}},
+	} {
+		for name2, tc2 := range map[string]struct {
+			ok bool
+		}{
+			"ok":     {true},
+			"not-ok": {false},
+		} {
+			t.Run(fmt.Sprintf("%s/%s", name1, name2), func(t *testing.T) {
+				t.Parallel()
 
-	mockCtrl := gomock.NewController(t)
-	mockPP := mocks.NewMockPP(mockCtrl)
+				ms := make([]monitor.BasicMonitor, 0, 5)
+				mockCtrl := gomock.NewController(t)
+				mockPP := mocks.NewMockPP(mockCtrl)
 
-	message := "aloha"
+				msg := monitor.Message{
+					OK:    tc2.ok,
+					Lines: tc1.lines,
+				}
 
-	for range 5 {
-		m := mocks.NewMockMonitor(mockCtrl)
-		m.EXPECT().Success(context.Background(), mockPP, message)
-		ms = append(ms, m)
+				for range 5 {
+					m := mocks.NewMockMonitor(mockCtrl)
+					m.EXPECT().Ping(context.Background(), mockPP, msg).Return(true)
+					ms = append(ms, m)
+				}
+
+				ok := monitor.NewComposed(ms...).Ping(context.Background(), mockPP, msg)
+				require.True(t, ok)
+			})
+		}
 	}
-
-	monitor.SuccessAll(context.Background(), mockPP, ms, message)
 }
 
-func TestStartAll(t *testing.T) {
+func TestComposedStart(t *testing.T) {
 	t.Parallel()
 
-	ms := make([]monitor.Monitor, 0, 5)
+	ms := make([]monitor.BasicMonitor, 0, 5)
 
 	mockCtrl := gomock.NewController(t)
 	mockPP := mocks.NewMockPP(mockCtrl)
@@ -61,55 +98,18 @@ func TestStartAll(t *testing.T) {
 
 	for range 5 {
 		m := mocks.NewMockMonitor(mockCtrl)
-		m.EXPECT().Start(context.Background(), mockPP, message)
+		m.EXPECT().Start(context.Background(), mockPP, message).Return(true)
 		ms = append(ms, m)
 	}
 
-	monitor.StartAll(context.Background(), mockPP, ms, message)
+	ok := monitor.NewComposed(ms...).Start(context.Background(), mockPP, message)
+	require.True(t, ok)
 }
 
-func TestFailureAll(t *testing.T) {
+func TestComposedExit(t *testing.T) {
 	t.Parallel()
 
-	ms := make([]monitor.Monitor, 0, 5)
-
-	mockCtrl := gomock.NewController(t)
-	mockPP := mocks.NewMockPP(mockCtrl)
-
-	message := "secret"
-
-	for range 5 {
-		m := mocks.NewMockMonitor(mockCtrl)
-		m.EXPECT().Failure(context.Background(), mockPP, message)
-		ms = append(ms, m)
-	}
-
-	monitor.FailureAll(context.Background(), mockPP, ms, message)
-}
-
-func TestLogAll(t *testing.T) {
-	t.Parallel()
-
-	ms := make([]monitor.Monitor, 0, 5)
-
-	mockCtrl := gomock.NewController(t)
-	mockPP := mocks.NewMockPP(mockCtrl)
-
-	message := "forest"
-
-	for range 5 {
-		m := mocks.NewMockMonitor(mockCtrl)
-		m.EXPECT().Log(context.Background(), mockPP, message)
-		ms = append(ms, m)
-	}
-
-	monitor.LogAll(context.Background(), mockPP, ms, message)
-}
-
-func TestExitStatusAll(t *testing.T) {
-	t.Parallel()
-
-	ms := make([]monitor.Monitor, 0, 5)
+	ms := make([]monitor.BasicMonitor, 0, 5)
 
 	mockCtrl := gomock.NewController(t)
 	mockPP := mocks.NewMockPP(mockCtrl)
@@ -118,14 +118,15 @@ func TestExitStatusAll(t *testing.T) {
 
 	for range 5 {
 		m := mocks.NewMockMonitor(mockCtrl)
-		m.EXPECT().ExitStatus(context.Background(), mockPP, 42, message)
+		m.EXPECT().Exit(context.Background(), mockPP, message).Return(true)
 		ms = append(ms, m)
 	}
 
-	monitor.ExitStatusAll(context.Background(), mockPP, ms, 42, message)
+	ok := monitor.NewComposed(ms...).Exit(context.Background(), mockPP, message)
+	require.True(t, ok)
 }
 
-func TestPingMessageAll(t *testing.T) {
+func TestComposedLog(t *testing.T) {
 	t.Parallel()
 
 	for name1, tc1 := range map[string]struct {
@@ -136,84 +137,39 @@ func TestPingMessageAll(t *testing.T) {
 		"one":   {[]string{"hi"}},
 		"two":   {[]string{"hi", "hey"}},
 	} {
-		monitorMessage := strings.Join(tc1.lines, "\n")
-
 		for name2, tc2 := range map[string]struct {
 			ok bool
 		}{
-			"ok":    {true},
-			"notok": {false},
+			"ok":     {true},
+			"not-ok": {false},
 		} {
 			t.Run(fmt.Sprintf("%s/%s", name1, name2), func(t *testing.T) {
 				t.Parallel()
 
-				ms := make([]monitor.Monitor, 0, 5)
+				ms := make([]monitor.BasicMonitor, 0, 5)
 				mockCtrl := gomock.NewController(t)
 				mockPP := mocks.NewMockPP(mockCtrl)
 
-				for range 5 {
+				msg := monitor.Message{
+					OK:    tc2.ok,
+					Lines: tc1.lines,
+				}
+
+				for range 3 {
 					m := mocks.NewMockMonitor(mockCtrl)
-					if tc2.ok {
-						m.EXPECT().Success(context.Background(), mockPP, monitorMessage)
-					} else {
-						m.EXPECT().Failure(context.Background(), mockPP, monitorMessage)
+					m.EXPECT().Log(context.Background(), mockPP, msg).Return(true)
+					ms = append(ms, m)
+				}
+				for range 2 {
+					m := mocks.NewMockBasicMonitor(mockCtrl)
+					if !tc2.ok {
+						m.EXPECT().Ping(context.Background(), mockPP, msg).Return(true)
 					}
 					ms = append(ms, m)
 				}
 
-				msg := message.MonitorMessage{
-					OK:    tc2.ok,
-					Lines: tc1.lines,
-				}
-				monitor.PingMessageAll(context.Background(), mockPP, ms, msg)
-			})
-		}
-	}
-}
-
-func TestLogMessageAll(t *testing.T) {
-	t.Parallel()
-
-	for name1, tc1 := range map[string]struct {
-		lines []string
-	}{
-		"nil":   {nil},
-		"empty": {[]string{}},
-		"one":   {[]string{"hi"}},
-		"two":   {[]string{"hi", "hey"}},
-	} {
-		monitorMessage := strings.Join(tc1.lines, "\n")
-
-		for name2, tc2 := range map[string]struct {
-			ok bool
-		}{
-			"ok":    {true},
-			"notok": {false},
-		} {
-			t.Run(fmt.Sprintf("%s/%s", name1, name2), func(t *testing.T) {
-				t.Parallel()
-
-				ms := make([]monitor.Monitor, 0, 5)
-				mockCtrl := gomock.NewController(t)
-				mockPP := mocks.NewMockPP(mockCtrl)
-
-				for range 5 {
-					m := mocks.NewMockMonitor(mockCtrl)
-					switch {
-					case tc2.ok && len(monitorMessage) > 0:
-						m.EXPECT().Log(context.Background(), mockPP, monitorMessage)
-					case tc2.ok:
-					default: // !tc.ok
-						m.EXPECT().Failure(context.Background(), mockPP, monitorMessage)
-					}
-					ms = append(ms, m)
-				}
-
-				msg := message.MonitorMessage{
-					OK:    tc2.ok,
-					Lines: tc1.lines,
-				}
-				monitor.LogMessageAll(context.Background(), mockPP, ms, msg)
+				ok := monitor.NewComposed(ms...).Log(context.Background(), mockPP, msg)
+				require.True(t, ok)
 			})
 		}
 	}

internal/notifier/composite_test.go
@@ -1,83 +1,0 @@
-package notifier_test
-
-import (
-	"context"
-	"strings"
-	"testing"
-
-	"go.uber.org/mock/gomock"
-
-	"github.com/favonia/cloudflare-ddns/internal/message"
-	"github.com/favonia/cloudflare-ddns/internal/mocks"
-	"github.com/favonia/cloudflare-ddns/internal/notifier"
-)
-
-func TestDescribeAll(t *testing.T) {
-	t.Parallel()
-
-	ms := make([]notifier.Notifier, 0, 5)
-
-	mockCtrl := gomock.NewController(t)
-
-	for range 5 {
-		m := mocks.NewMockNotifier(mockCtrl)
-		m.EXPECT().Describe(gomock.Any())
-		ms = append(ms, m)
-	}
-
-	callback := func(_service, _params string) { /* the callback content is not relevant here. */ }
-	notifier.DescribeAll(callback, ms)
-}
-
-func TestSendAll(t *testing.T) {
-	t.Parallel()
-
-	ms := make([]notifier.Notifier, 0, 5)
-
-	mockCtrl := gomock.NewController(t)
-	mockPP := mocks.NewMockPP(mockCtrl)
-
-	message := "aloha"
-
-	for range 5 {
-		m := mocks.NewMockNotifier(mockCtrl)
-		m.EXPECT().Send(context.Background(), mockPP, message)
-		ms = append(ms, m)
-	}
-
-	notifier.SendAll(context.Background(), mockPP, ms, message)
-}
-
-func TestSendMessageAll(t *testing.T) {
-	t.Parallel()
-
-	for name1, tc1 := range map[string]struct {
-		notifierMessages []string
-	}{
-		"nil":   {nil},
-		"empty": {[]string{}},
-		"one":   {[]string{"hi"}},
-		"two":   {[]string{"hi", "hey"}},
-	} {
-		notifierMessage := strings.Join(tc1.notifierMessages, " ")
-
-		t.Run(name1, func(t *testing.T) {
-			t.Parallel()
-
-			ns := make([]notifier.Notifier, 0, 5)
-			mockCtrl := gomock.NewController(t)
-			mockPP := mocks.NewMockPP(mockCtrl)
-
-			for range 5 {
-				n := mocks.NewMockNotifier(mockCtrl)
-				if len(tc1.notifierMessages) > 0 {
-					n.EXPECT().Send(context.Background(), mockPP, notifierMessage)
-				}
-				ns = append(ns, n)
-			}
-
-			msg := message.NotifierMessage(tc1.notifierMessages)
-			notifier.SendMessageAll(context.Background(), mockPP, ns, msg)
-		})
-	}
-}

commit 1f6a5483e4aa844c7e95ca67ffa440a2814a05b7

fix: nil pointer bug introduced by commit 319e6a0 (issue #911)

internal/config/config.go
@@ -59,7 +59,7 @@ func Default() *Config {
 		WAFListDescription: "",
 		DetectionTimeout:   time.Second * 5,  //nolint:mnd
 		UpdateTimeout:      time.Second * 30, //nolint:mnd
-		Monitor:            nil,
-		Notifier:           nil,
+		Monitor:            monitor.NewComposed(),
+		Notifier:           notifier.NewComposed(),
 	}
 }

internal/config/config_print.go
@@ -85,15 +85,23 @@ func (c *Config) Print(ppfmt pp.PP) {
 	item("Record/list updating:", "%v", c.UpdateTimeout)
 
 	if c.Monitor != nil {
-		section("Monitors:")
+		count := 0
 		for name, params := range c.Monitor.Describe {
+			count++
+			if count == 1 {
+				section("Monitors:")
+			}
 			item(name+":", "%s", params)
 		}
 	}
 
 	if c.Notifier != nil {
-		section("Notification services (via shoutrrr):")
+		count := 0
 		for name, params := range c.Notifier.Describe {
+			count++
+			if count == 1 {
+				section("Notification services (via shoutrrr):")
+			}
 			item(name+":", "%s", params)
 		}
 	}

internal/config/env_notifier.go
@@ -18,6 +18,6 @@ func ReadAndAppendShoutrrrURL(ppfmt pp.PP, key string, field *notifier.Notifier)
 	}
 
 	// Append the new monitor to the existing list
-	*field = s
+	*field = notifier.NewComposed(*field, s)
 	return true
 }

internal/monitor/composite.go
@@ -6,28 +6,29 @@ import (
 	"github.com/favonia/cloudflare-ddns/internal/pp"
 )
 
-type monitors []BasicMonitor
+// Composed represents the composite of multiple monitors.
+type Composed []BasicMonitor
 
-var _ Monitor = monitors{}
+var _ Monitor = Composed{}
 
 // NewComposed creates a new composed monitor.
-func NewComposed(mons ...BasicMonitor) monitors {
+func NewComposed(mons ...BasicMonitor) Composed {
 	ms := make([]BasicMonitor, 0, len(mons))
 	for _, m := range mons {
 		if m == nil {
 			continue
 		}
-		if list, composed := m.(monitors); composed {
+		if list, composed := m.(Composed); composed {
 			ms = append(ms, list...)
 		} else {
 			ms = append(ms, m)
 		}
 	}
-	return monitors(ms)
+	return Composed(ms)
 }
 
 // Describe calls [Monitor.Describe] for each monitor in the group with the callback.
-func (ms monitors) Describe(yield func(name string, params string) bool) {
+func (ms Composed) Describe(yield func(name string, params string) bool) {
 	for _, m := range ms {
 		for name, params := range m.Describe {
 			if !yield(name, params) {
@@ -38,7 +39,7 @@ func (ms monitors) Describe(yield func(name string, params string) bool) {
 }
 
 // Ping calls [Monitor.Ping] for each monitor in the group.
-func (ms monitors) Ping(ctx context.Context, ppfmt pp.PP, message Message) bool {
+func (ms Composed) Ping(ctx context.Context, ppfmt pp.PP, message Message) bool {
 	ok := true
 	for _, m := range ms {
 		ok = ok && m.Ping(ctx, ppfmt, message)
@@ -47,7 +48,7 @@ func (ms monitors) Ping(ctx context.Context, ppfmt pp.PP, message Message) bool 
 }
 
 // Start calls [Monitor.Start] for each monitor in ms.
-func (ms monitors) Start(ctx context.Context, ppfmt pp.PP, message string) bool {
+func (ms Composed) Start(ctx context.Context, ppfmt pp.PP, message string) bool {
 	ok := true
 	for _, m := range ms {
 		if em, extended := m.(Monitor); extended {
@@ -58,7 +59,7 @@ func (ms monitors) Start(ctx context.Context, ppfmt pp.PP, message string) bool 
 }
 
 // Exit calls [Monitor.Exit] for each monitor in ms.
-func (ms monitors) Exit(ctx context.Context, ppfmt pp.PP, message string) bool {
+func (ms Composed) Exit(ctx context.Context, ppfmt pp.PP, message string) bool {
 	ok := true
 	for _, m := range ms {
 		if em, extended := m.(Monitor); extended {
@@ -69,7 +70,7 @@ func (ms monitors) Exit(ctx context.Context, ppfmt pp.PP, message string) bool {
 }
 
 // Log calls [Monitor.Log] for each monitor in the group.
-func (ms monitors) Log(ctx context.Context, ppfmt pp.PP, msg Message) bool {
+func (ms Composed) Log(ctx context.Context, ppfmt pp.PP, msg Message) bool {
 	ok := true
 	for _, m := range ms {
 		if em, extended := m.(Monitor); extended {

internal/notifier/composite.go
@@ -1,0 +1,48 @@
+package notifier
+
+import (
+	"context"
+
+	"github.com/favonia/cloudflare-ddns/internal/pp"
+)
+
+// Composed represents the composite of multiple notifiers.
+type Composed []Notifier
+
+var _ Notifier = Composed{}
+
+// NewComposed creates a new composed notifier.
+func NewComposed(nots ...Notifier) Composed {
+	ns := make([]Notifier, 0, len(nots))
+	for _, n := range nots {
+		if n == nil {
+			continue
+		}
+		if list, composed := n.(Composed); composed {
+			ns = append(ns, list...)
+		} else {
+			ns = append(ns, n)
+		}
+	}
+	return Composed(ns)
+}
+
+// Describe calls [Notifier.Describe] for each notifier in the group with the callback.
+func (ns Composed) Describe(yield func(name string, params string) bool) {
+	for _, n := range ns {
+		for name, params := range n.Describe {
+			if !yield(name, params) {
+				return
+			}
+		}
+	}
+}
+
+// Send calls [Notifier.Send] for each notifier in the group.
+func (ns Composed) Send(ctx context.Context, ppfmt pp.PP, msg Message) bool {
+	ok := true
+	for _, n := range ns {
+		ok = ok && n.Send(ctx, ppfmt, msg)
+	}
+	return ok
+}

internal/config/config_test.go
@@ -13,3 +13,15 @@ func TestDefaultConfigNotNil(t *testing.T) {
 
 	require.NotNil(t, config.Default())
 }
+
+func TestDefaultConfigMonitorNotNil(t *testing.T) {
+	t.Parallel()
+
+	require.NotNil(t, config.Default().Monitor)
+}
+
+func TestDefaultConfigoNotifierNotNil(t *testing.T) {
+	t.Parallel()
+
+	require.NotNil(t, config.Default().Notifier)
+}

internal/config/env_notifier_test.go
@@ -47,7 +47,10 @@ func TestReadAndAppendShoutrrrURL(t *testing.T) {
 			nil,
 			func(t *testing.T, n not) {
 				t.Helper()
-				s, ok := n.(notifier.Shoutrrr)
+				ns, ok := n.(notifier.Composed)
+				require.True(t, ok)
+				require.Len(t, ns, 1)
+				s, ok := ns[0].(notifier.Shoutrrr)
 				require.True(t, ok)
 				require.Equal(t, []string{"Generic"}, s.ServiceDescriptions)
 			},
@@ -71,7 +74,10 @@ func TestReadAndAppendShoutrrrURL(t *testing.T) {
 			nil,
 			func(t *testing.T, n not) {
 				t.Helper()
-				s, ok := n.(notifier.Shoutrrr)
+				ns, ok := n.(notifier.Composed)
+				require.True(t, ok)
+				require.Len(t, ns, 1)
+				s, ok := ns[0].(notifier.Shoutrrr)
 				require.True(t, ok)
 				require.Equal(t, []string{"Generic", "Pushover"}, s.ServiceDescriptions)
 			},

internal/monitor/composite_test.go
@@ -17,7 +17,7 @@ func TestComposedDescribe(t *testing.T) {
 
 	mockCtrl := gomock.NewController(t)
 
-	ms1 := make([]monitor.BasicMonitor, 0, 5)
+	ms1 := make([]monitor.BasicMonitor, 0, 3)
 	for range 3 {
 		m := mocks.NewMockMonitor(mockCtrl)
 		m.EXPECT().Describe(gomock.Any()).DoAndReturn(
@@ -27,7 +27,7 @@ func TestComposedDescribe(t *testing.T) {
 		)
 		ms1 = append(ms1, m)
 	}
-	ms2 := make([]monitor.BasicMonitor, 0, 5)
+	ms2 := make([]monitor.BasicMonitor, 0, 2)
 	for range 2 {
 		m := mocks.NewMockMonitor(mockCtrl)
 		ms2 = append(ms2, m)

internal/notifier/composite_test.go
@@ -1,0 +1,76 @@
+package notifier_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+
+	"github.com/favonia/cloudflare-ddns/internal/mocks"
+	"github.com/favonia/cloudflare-ddns/internal/notifier"
+)
+
+func TestComposedDescribe(t *testing.T) {
+	t.Parallel()
+
+	mockCtrl := gomock.NewController(t)
+
+	ns1 := make([]notifier.Notifier, 0, 3)
+	for range 3 {
+		n := mocks.NewMockNotifier(mockCtrl)
+		n.EXPECT().Describe(gomock.Any()).DoAndReturn(
+			func(yield func(string, string) bool) {
+				yield("name", "params")
+			},
+		)
+		ns1 = append(ns1, n)
+	}
+	ns2 := make([]notifier.Notifier, 0, 2)
+	for range 2 {
+		n := mocks.NewMockNotifier(mockCtrl)
+		ns2 = append(ns2, n)
+	}
+	c := notifier.NewComposed(notifier.NewComposed(ns1...), notifier.NewComposed(ns2...))
+
+	count := 0
+	for range c.Describe {
+		count++
+		if count >= 3 {
+			break
+		}
+	}
+	require.Equal(t, 3, count)
+}
+
+func TestComposedSend(t *testing.T) {
+	t.Parallel()
+
+	for name, tc := range map[string]struct {
+		ss []string
+	}{
+		"nil":   {nil},
+		"empty": {[]string{}},
+		"one":   {[]string{"hi"}},
+		"two":   {[]string{"hi", "hey"}},
+	} {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			ns := make([]notifier.Notifier, 0, 5)
+			mockCtrl := gomock.NewController(t)
+			mockPP := mocks.NewMockPP(mockCtrl)
+
+			msg := notifier.Message(tc.ss)
+
+			for range 5 {
+				n := mocks.NewMockNotifier(mockCtrl)
+				n.EXPECT().Send(context.Background(), mockPP, msg).Return(true)
+				ns = append(ns, n)
+			}
+
+			ok := notifier.NewComposed(ns...).Send(context.Background(), mockPP, msg)
+			require.True(t, ok)
+		})
+	}
+}