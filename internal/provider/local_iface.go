4518faca43c375545ee3dd6828b571b327579b6b
favonia <favonia@gmail.com>
2024-09-26 23:08:30 -0500

    fix(config): change `local:` to `local.iface:` (#947)

--- internal/provider/local_iface.go
+++ internal/provider/local_iface.go
@@ -5,7 +5,7 @@ import "github.com/favonia/cloudflare-ddns/internal/provider/protocol"
 // NewLocalWithInterface creates a protocol.LocalWithInterface provider.
 func NewLocalWithInterface(iface string) Provider {
 	return protocol.LocalWithInterface{
-		ProviderName:  "local:" + iface,
+		ProviderName:  "local.iface:" + iface,
 		InterfaceName: iface,
 	}
 }
