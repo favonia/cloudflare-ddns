7afb1d2254a810ace2cbbb0d5d0ea737bb1c0c4d
favonia <favonia@gmail.com>
2024-11-21 12:09:36 -0600

    feat(provider): new provider cloudflare.trace:URL (#988)

--- internal/provider/cloudflare_trace.go
+++ internal/provider/cloudflare_trace.go
@@ -29,3 +29,21 @@ func NewCloudflareTrace() Provider {
 		},
 	})
 }
+
+// NewCloudflareTraceCustom creates a specialized CloudflareTrace provider
+// with a specific URL.
+func NewCloudflareTraceCustom(url string) Provider {
+	return NewHappyEyeballs(protocol.Regexp{
+		ProviderName: "cloudflare.trace",
+		Param: map[ipnet.Type]protocol.RegexpParam{
+			ipnet.IP4: {
+				protocol.Constant(url),
+				fieldIP,
+			},
+			ipnet.IP6: {
+				protocol.Constant(url),
+				fieldIP,
+			},
+		},
+	})
+}
