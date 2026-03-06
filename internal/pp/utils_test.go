d1230f55307c2143e6c9415025940f7195e611d0
favonia <favonia@gmail.com>
2026-03-06 11:50:50 +0800

    test(pp): add more tests

--- internal/pp/utils_test.go
+++ internal/pp/utils_test.go
@@ -1,6 +1,7 @@
 package pp_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -40,6 +41,46 @@ func TestEnglishJoin(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 			require.Equal(t, tc.output, pp.EnglishJoin(tc.input))
+		})
+	}
+}
+
+func TestJoinMap(t *testing.T) {
+	t.Parallel()
+
+	for name, tc := range map[string]struct {
+		input  []int
+		output string
+	}{
+		"none":  {nil, "(none)"},
+		"one":   {[]int{1}, "n=1"},
+		"three": {[]int{1, 2, 3}, "n=1, n=2, n=3"},
+	} {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			require.Equal(t, tc.output, pp.JoinMap(func(v int) string {
+				return fmt.Sprintf("n=%d", v)
+			}, tc.input))
+		})
+	}
+}
+
+func TestEnglishJoinMap(t *testing.T) {
+	t.Parallel()
+
+	for name, tc := range map[string]struct {
+		input  []int
+		output string
+	}{
+		"none":  {nil, "(none)"},
+		"one":   {[]int{1}, "n=1"},
+		"three": {[]int{1, 2, 3}, "n=1, n=2, and n=3"},
+	} {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			require.Equal(t, tc.output, pp.EnglishJoinMap(func(v int) string {
+				return fmt.Sprintf("n=%d", v)
+			}, tc.input))
 		})
 	}
 }
