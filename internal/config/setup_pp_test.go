78ffc027adb27aa0e6937a22fec43c9b8be0f58b
favonia <favonia@gmail.com>
2026-03-27 22:07:51 -0500

    fix: keep SetupPP usable on parse failure

--- internal/config/setup_pp_test.go
+++ internal/config/setup_pp_test.go
@@ -39,16 +39,20 @@ func TestSetupPP(t *testing.T) {
 			var buf strings.Builder
 			ppfmt, ok := config.SetupPP(&buf)
 			require.Equal(t, tc.ok, ok)
+			require.NotZero(t, ppfmt)
 
 			switch {
 			case ok:
-				require.NotZero(t, ppfmt)
 				ppfmt.Infof(pp.EmojiStar, "info")
 				ppfmt.Noticef(pp.EmojiStar, "notice")
 				require.Equal(t, tc.output, buf.String())
 			case !ok:
-				require.Zero(t, ppfmt)
-				require.Equal(t, tc.output, buf.String())
+				ppfmt.Infof(pp.EmojiBye, "Bye!")
+				if tc.valEmoji == "false" {
+					require.Equal(t, tc.output+"Bye!\n", buf.String())
+				} else {
+					require.Equal(t, tc.output+"👋 Bye!\n", buf.String())
+				}
 			}
 		})
 	}
