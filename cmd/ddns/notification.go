19aabcf8655e04c47931528e468a55744e1c0eca
favonia <favonia@gmail.com>
2026-07-26 16:37:56 +0800

    docs: explain scheduling failure input

--- cmd/ddns/notification.go
+++ cmd/ddns/notification.go
@@ -11,6 +11,8 @@ func startupNotification() notifier.Notification {
 	return notifier.NewNotificationf(notifier.KindStartup, "Cloudflare DDNS has started.")
 }
 
+// schedulingFailureNotification adds the given schedule to the message.
+// The schedule is the original UPDATE_CRON string.
 func schedulingFailureNotification(schedule string) notifier.Notification {
 	return notifier.NewNotificationf(notifier.KindSchedulingFailure,
 		"Cloudflare DDNS stopped because no updates are scheduled in the near future. "+
